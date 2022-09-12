b20684df20451a34d85ebfc7f2dfce8d1d8c9512
Leticia Patrocinio <leticia_patrocinio@outlook.com>
2022-09-12 17:21:39 -0300

    Correções de parâmetros

--- ac01.py
+++ ac01.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
-app.config['MYSQL_DATABASE_DB'] = 'teste'
+app.config['MYSQL_DATABASE_DB'] = 'alunos'
 app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
@@ -42,5 +42,5 @@ def listar():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5002))
+    port = int(os.environ.get("PORT", 5008))
     app.run(host='0.0.0.0', port=port)
